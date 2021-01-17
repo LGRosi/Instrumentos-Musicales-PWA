f1921dee5b36c4d7f79b3b9a1cfa3f91c634c96c
LGRosi <66270940+LGRosi@users.noreply.github.com>
2021-01-17 15:14:15 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,79 @@
+
+var app = new Vue ({
+	el: ".cambios",
+	data: {
+			titulo:"Instrumentos musicales",
+
+			ver: false,
+
+			textoGris: "textogris",
+			pPagoSeguro: "tamañotextogris",
+
+			h2Slider1: "Guitarras eléctricas",
+			h2Slider2: "Instrumentos internacionales",
+			h2Slider3: "Equipos de sonido",
+
+			pSlider1:"Nuestra mejor y más solicitada selección de guitarras.",
+			pSlider2:"Los mejores instrumentos con la más alta calidad.",
+			pSlider3:"Te ofrecemos las mejores marcas de audio reconocidas mundialmente.",
+
+			imgSlider1Pc: "imagenes/slider1-pc.jpg",
+			imgSlider1Tablet: "imagenes/slider1-tablet.jpg",
+			imgSlider1Celular: "imagenes/slider1-celular.jpg",
+
+			imgSlider2Pc: "imagenes/slider2-pc.jpg",
+			imgSlider2Tablet: "imagenes/slider2-tablet.jpg",
+			imgSlider2Celular: "imagenes/slider2-celular.jpg",
+
+			imgSlider3Pc: "imagenes/slider3-pc.jpg",
+			imgSlider3Tablet: "imagenes/slider3-tablet.jpg",
+			imgSlider3Celular: "imagenes/slider3-celular.jpg",
+
+			imgPagoSeguro: "imagenes/pago.png",
+			imgEnvioGlobal: "imagenes/mundo.png",
+			imgEntregaRapida: "imagenes/envio.png",
+
+			imgBandaPc: "imagenes/banda_pc.jpg",
+			imgBandaTablet: "imagenes/banda_tablet.jpg",
+			imgBandaCelular: "imagenes/banda_celular.jpg",
+
+			imgBajoPc: "imagenes/bajo_pc.jpg",
+			imgBajoTablet: "imagenes/bajo_tablet.jpg",
+			imgBajoCelular: "imagenes/bajo_celular.jpg",
+
+			imgBateriaPc: "imagenes/bateria_pc.jpg",
+			imgBateriaTablet: "imagenes/bateria_tablet.jpg",
+			imgBateriaCelular: "imagenes/bateria_celular.jpg",
+
+			imgGuitarraPc: "imagenes/guitarra_pc.jpg",
+			imgGuitarraTablet: "imagenes/guitarra_tablet.jpg",
+			imgGuitarraCelular: "imagenes/guitarra_celular.jpg",
+
+			imgSaxoPc: "imagenes/saxo_pc.jpg",
+			imgSaxoTablet: "imagenes/saxo_tablet.jpg",
+			imgSaxoCelular: "imagenes/saxo_celular.jpg",
+
+			imgGuitarraElectricaPc: "imagenes/guitarraElectrica_pc.jpg",
+			imgGuitarraElectricaTablet: "imagenes/guitarraElectrica_tablet.jpg",
+			imgGuitarraElectricaCelular: "imagenes/guitarraElectrica_celular.jpg",
+
+			imgSintetizadorPc: "imagenes/sintetizador_pc.jpg",
+			imgSintetizadorTablet: "imagenes/sintetizador_tablet.jpg",
+			imgSintetizadorCelular: "imagenes/sintetizador_celular.jpg",
+
+			imgAsidePc: "imagenes/imgAside_pc.jpg",
+			imgAsideTablet: "imagenes/imgAside_tablet.jpg",
+			imgAsideCelular: "imagenes/imgAside_celular.jpg",
+
+			datosFor:["MUSINDI", "Calle falsa 123 - CABA", "Ciudad de Buenos Aires - Argentina",
+			"Llamanos: 11 9999 9999", "Envianos un correo electrónico:", "instrumentos@Musindi.com.ar"],
+
+			beneficios:["Tenemos instrumentos de la más alta calidad.", "Manejamos marcas mundialmente reconocidas.",
+			"Contamos con repación y repuestos.", "Tenés un 35% de descuento en tu primera compra.",
+			"Te proporcionamos envíos nacionales sin costo."],
+
+			
+ 			
+		}
+})
+
